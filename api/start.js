d940360e3b823390e00d7deeebc8e3b9154e1443
ramanydv795 <raman@gmail.com>
2026-04-28 00:02:37 +0530

    trigger redeploy

--- api/start.js
+++ api/start.js
@@ -5,7 +5,7 @@ export default function handler(req, res) {
     return res.status(200).json({ success: true });
   }
   return res.status(405).json({ error: "Method not allowed" });
-}
+
 
   try {
     const client = new OpenAI({
